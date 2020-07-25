291ba33d13e18ddf0044f2ea4d15aadeda90a868
Darwin Morocho <darwin@iMac-de-Darwin.local>
2020-07-25 09:12:39 -0500

    added setTimeout

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,9 @@ module.exports = {
   apps: [
     {
       name: 'backend',
-      script: './dist/index.js'
+      script: './dist/index.js',
+      instances: 2,
+      exec_mode: 'cluster'
     }
   ]
 };
