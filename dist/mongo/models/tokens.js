6dd7f29aed39a57478c5c461674123e18dc76f91
Darwin Morocho <darwin@iMac-de-Darwin.local>
2020-07-25 08:16:07 -0500

    removed dist

--- dist/mongo/models/tokens.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-var schema = new mongoose_1.Schema({
-    jwt: { type: String, require: true, unique: true },
-    payload: { type: Object }
-});
-var Tokes = mongoose_1.model('token', schema);
-exports.default = Tokes;
